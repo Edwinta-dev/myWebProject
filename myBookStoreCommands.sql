89841f3bd0af2038304d0af137cbeef9ca0a9a45
Edwinta-dev <145195461+Edwinta-dev@users.noreply.github.com>
2025-02-28 11:14:33 +0800

    Add files via upload
    Importing existing project files from initial tutorial practices

---
+++ myBookStoreCommands.sql
@@ -0,0 +1,22 @@
+select * from book;
+insert into book values (1001, 'Java for Dummies' , 11.11 , 11);
+insert into book values (1002 ,' Only Java' , 22.22 , 22);
+insert into book values (1003 , 'Java ABC' , 33.33 , 33);
+insert into book values (1004 ,'Java 123' , 44.44 , 44);
+select * from book_author;
+insert into book_author values (1 , 1001);
+insert into book_author values (2 , 1001);
+insert into book_author values (3 , 1001);
+insert into book_author values (1 , 1002);
+insert into book_author values (3 , 1002);
+insert into book_author values (2 , 1003);
+insert into book_author values (2 , 1004);
+select * from author;
+insert into author values (1 , 'Tan Ah Teck' , 'teck@nowhere.com');
+insert into author values (2 , 'Mohamed Ali' , 'ali@somewhere.com');
+insert into author values (3 , 'Kumar', 'kumar@abc.com');
+insert into author values (4 , 'Kelvin Jones', 'kelvin@xyz.com');
+
+
+
+
---
+++ productCommands.sql
@@ -0,0 +1,5 @@
+insert into product values (1001,'PEN','Pen Red',5000,1.23);
+insert into product values (1002,'PEN','Pen Blue',8000,1.25);
+insert into product values (1003,'PEN','Pen Black',2000,1.25);
+insert into product values (1004,'PCL','Pencil 2B',10000,0.49);
+insert into product values (1005,'PCL','Pencil 2H',9000,0.48);
